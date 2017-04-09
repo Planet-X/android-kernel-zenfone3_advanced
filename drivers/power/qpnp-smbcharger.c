00328e820a08a844c1333440541ef81cc3046fcc
Sultan Alsawaf <sultanxda@gmail.com>
2017-04-09 15:38:02 -0700

    treewide: Properly fix use of uninitialized variables
    While most of these were harmless, there were several dangerous instances
    of uninitialized variable usage that influenced functionality. There were
    quite a few instances of explicit uninitialized pointer dereferences, which
    can cause kernel panics.
    
    This patch is for the Oreo kernel.
    
    Change-Id: If51e7d1298ad755c35fa860ebe730588f5cc30d8
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -6061,7 +6061,8 @@ static bool ADF_check_status(void)
 void jeita_judge_work(struct work_struct *work)
 {
 	int state;
-	int rc, ret;
+	int rc = 0;
+	int ret = 0;
 	int batt_volt = 4000;
 	u8 FV_reg;
 	u8 FV_CFG_reg_value = 0;	//set float_voltage_reg_value
@@ -6295,7 +6296,8 @@ void adapter_detect_judge_work(struct work_struct *work)
 {
 	char *usb_type_name = "null";
 	enum power_supply_type usb_supply_type;
-	int rc, ret;
+	int rc = 0;
+	int ret = 0;
 	u8 reg;
 	int typec_current;
 	bool DCP_demo_app_flag;
