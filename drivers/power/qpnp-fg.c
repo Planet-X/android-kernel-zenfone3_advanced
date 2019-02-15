31bb7eddb4f59d3c904386bc3836634475ba3612
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2019-02-15 15:26:12 +0000

    power: qpnp-fg: Fix uninitialized DIG_MAJOR variable

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -9178,8 +9178,7 @@ static int fg_memif_init(struct fg_chip *chip)
 		return rc;
 	}
 
-	dig_major = chip->revision[DIG_MAJOR];
-	switch (dig_major) {
+	switch (chip->revision[DIG_MAJOR]) {
 	case DIG_REV_1:
 	case DIG_REV_2:
 		chip->offset = offset[0].address;
