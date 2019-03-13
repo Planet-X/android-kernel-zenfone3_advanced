874cfff50810692c42fce7234aae45bd9993f155
Planet-X <planetx.dev@gmail.com>
2019-03-13 12:57:25 +0100

    Revert "f2fs: rapidly GC all invalid blocks when screen goes off & plugged in"
    This reverts commit 7e25bb9d138a4c08b14c822bdb48a0eaec0eb928.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1033,7 +1033,6 @@ static void f2fs_put_super(struct super_block *sb)
 
 	/* f2fs_write_checkpoint can update stat informaion */
 	f2fs_destroy_stats(sbi);
-	f2fs_sbi_list_del(sbi);
 
 	/*
 	 * normally superblock is clean, so we need to release this.
@@ -1375,6 +1374,7 @@ static void default_options(struct f2fs_sb_info *sbi)
 	F2FS_OPTION(sbi).test_dummy_encryption = false;
 	sbi->readdir_ra = 1;
 
+	set_opt(sbi, BG_GC);
 	set_opt(sbi, INLINE_XATTR);
 	set_opt(sbi, INLINE_DATA);
 	set_opt(sbi, INLINE_DENTRY);
@@ -2981,8 +2981,6 @@ try_onemore:
 	if (err)
 		goto free_node_inode;
 
-	f2fs_sbi_list_add(sbi);
-
 	/* read root inode and dentry */
 	root = f2fs_iget(sb, F2FS_ROOT_INO(sbi));
 	if (IS_ERR(root)) {
@@ -3120,9 +3118,6 @@ free_node_inode:
 	f2fs_release_ino_entry(sbi, true);
 	truncate_inode_pages_final(NODE_MAPPING(sbi));
 	iput(sbi->node_inode);
-	mutex_unlock(&sbi->umount_mutex);
-	f2fs_sbi_list_del(sbi);
-	f2fs_destroy_stats(sbi);
 free_nm:
 	f2fs_destroy_node_manager(sbi);
 free_sm:
