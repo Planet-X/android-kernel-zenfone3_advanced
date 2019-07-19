d0f8519ad645f2865d9567f844b0a61795191645
Planet-X <planetx.dev@gmail.com>
2019-07-19 14:08:33 +0200

    zip: Update to AnyKernel3
    Source:  https://github.com/osm0sis/AnyKernel3
    Version: bb013da8159011286e6ee38a98b86d84eaeb994a
    
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- zip/anykernel.sh
+++ zip/anykernel.sh
@@ -1,18 +1,19 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=planet9 @ xda-developers
+kernel.string=Advanced Kernel for Zenfone 3
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
+do.cleanuponabort=1
 device.name1=Z017
 device.name2=ASUS_Z017D_1
 device.name3=Z012
 device.name4=ASUS_Z012D
+supported.versions=8.1.0 - 9
 '; } # end properties
 
 # shell variables
@@ -23,13 +24,12 @@ ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
 
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
-chmod -R 755 $ramdisk/sbin;
 chown -R root:root $ramdisk/*;
 
 
@@ -39,11 +39,10 @@ dump_boot;
 # begin ramdisk changes
 
 # init.rc - Add spectrum support
+backup_file init.rc;
 insert_line init.rc "import /init.spectrum.rc" before "import /init.environ.rc" "import /init.spectrum.rc";
-
 # end ramdisk changes
 
 write_boot;
-
 ## end install
 
